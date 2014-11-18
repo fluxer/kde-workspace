fde6c98cb2b3d41f323026a36141432cd6430566
Ivailo Monev <xakepa10@gmail.com>
2014-11-18 16:52:08 +0000

    removed docs, man kioslave part and khelpcenter

--- khelpcenter/htmlsearch/Messages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-### TODO: why do we need 2 POT files for one directory?
-$XGETTEXT index.cpp htmlsearch.cpp progressdialog.cpp rc.cpp -o $podir/htmlsearch.pot
-$XGETTEXT kcmhtmlsearch.cpp klangcombo.cpp ktagcombobox.cpp rc.cpp -o $podir/kcmhtmlsearch.pot
--- khelpcenter/version.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef KHC_VERSION_H
-#define KHC_VERSION_H
-
-#define HELPCENTER_VERSION KDE_VERSION_STRING
-
-#endif // KHC_VERSION_H
-// vim:ts=2:sw=2:et
