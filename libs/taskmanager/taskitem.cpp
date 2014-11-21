dd8bb6a0f55c5d76facd1c871353be58144eaf5c
Ivailo Monev <xakepa10@gmail.com>
2014-11-21 23:59:03 +0000

    kactivities adjustments

--- libs/taskmanager/taskitem.cpp
+++ libs/taskmanager/taskitem.cpp
@@ -25,7 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // Own
 #include "taskitem.h"
 
-#include <kactivities/info.h>
+#include <KActivities/Info>
 #include <KConfig>
 #include <KConfigGroup>
 #include <KDebug>
