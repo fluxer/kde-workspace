96d7223e38a957ad0122eba532ff48f35dc3c31b
Ivailo Monev <xakepa10@gmail.com>
2014-12-09 18:20:21 +0000

    Messages.sh fixup

--- kcontrol/access/Messages.sh
+++ kcontrol/access/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ### TODO: why do we need 2 POT files for a single directory?
-$XGETTEXT kaccess.cpp main.cpp rc.cpp -o $podir/kaccess.pot
-$XGETTEXT kcmaccess.cpp rc.cpp -o $podir/kcmaccess.pot
+$XGETTEXT kaccess.cpp main.cpp -o $podir/kaccess.pot
+$XGETTEXT kcmaccess.cpp -o $podir/kcmaccess.pot
--- plasma/desktop/shell/Messages.sh
+++ plasma/desktop/shell/Messages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 $EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT *.cpp scripting/*.cpp activitymanager/*.cpp -o $podir/plasma-desktop.pot
+$XGETTEXT *.cpp scripting/*.cpp -o $podir/plasma-desktop.pot
 rm -f rc.cpp
