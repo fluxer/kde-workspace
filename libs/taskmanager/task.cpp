a9e7c62d4d31b6ebb5bfa9f116f3b6aca93921ff
Ivailo Monev <xakepa10@gmail.com>
2021-01-21 20:34:15 +0200

    libs: correct best icon for task logic
    patch from NetBSD's pkgsrc
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- libs/taskmanager/task.cpp
+++ libs/taskmanager/task.cpp
@@ -458,10 +458,10 @@ bool Task::idMatch(const QString& id1, const QString& id2)
     if (id1.isEmpty() || id2.isEmpty())
         return false;
 
-    if (id1.contains(id2) > 0)
+    if (id1.contains(id2))
         return true;
 
-    if (id2.contains(id1) > 0)
+    if (id2.contains(id1))
         return true;
 
     return false;
