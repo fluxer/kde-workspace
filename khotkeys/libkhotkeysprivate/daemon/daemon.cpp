869384d9b601a18bf743fdf76a364719da91cbd6
Ivailo Monev <xakepa10@gmail.com>
2021-03-02 04:31:59 +0200

    khotkeys: format
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- khotkeys/libkhotkeysprivate/daemon/daemon.cpp
+++ khotkeys/libkhotkeysprivate/daemon/daemon.cpp
@@ -34,50 +34,46 @@ namespace KHotKeys { namespace Daemon {
 
 
 bool isEnabled()
-    {
+{
     KConfig khotkeysrc( KHOTKEYS_CONFIG_FILE );
     KConfigGroup main( &khotkeysrc, "Main" );
     return ! main.readEntry( "Disabled", false );
-    }
+}
 
 static QDBusInterface* Kded()
-    {
+{
     QDBusInterface *iface = new QDBusInterface( "org.kde.kded", "/kded","org.kde.kded" );
-    if (!iface->isValid())
-        {
+    if (!iface->isValid()) {
         QDBusError err = iface->lastError();
-        if (err.isValid())
-            {
+        if (err.isValid()) {
             kError() << "Failed to contact kded [" << err.name() << "]:" << err.message();
-            }
         }
-    return iface;
     }
+    return iface;
+}
 
 
 bool isRunning()
-    {
+{
     std::auto_ptr<QDBusInterface> kded( Kded() );
-    if (!kded->isValid())
-        {
+    if (!kded->isValid()) {
         return false;
-        }
+    }
 
     // I started with checking if i could get a valid /KHotKeys Interface. But
     // it resisted to work. So lets do the other thing.
     QDBusReply<QStringList> modules = kded->call( "loadedModules" );
     return modules.value().contains("khotkeys");
-    }
+}
 
 
 bool reload()
-    {
+{
     // No kded no reload
     std::auto_ptr<QDBusInterface> kded( Kded() );
-    if (!kded->isValid())
-        {
+    if (!kded->isValid()) {
         return false;
-        }
+    }
 
     // Inform kdedkhotkeys demon to reload settings
     QDBusConnection bus = QDBusConnection::sessionBus();
@@ -86,93 +82,78 @@ bool reload()
         "/modules/khotkeys",
         "org.kde.khotkeys",
         bus );
-    if(!iface.isValid())
-        {
+    if(!iface.isValid()) {
         QDBusError err = iface.lastError();
-        if (err.isValid())
-            {
+        if (err.isValid()) {
             kError() << err.name() << ":" << err.message();
-            }
-        return start();
         }
+        return start();
+    }
 
     QDBusMessage reply = iface.call("reread_configuration");
     QDBusError err = iface.lastError();
-    if (err.isValid())
-        {
+    if (err.isValid()) {
         kError() << err.name() << ":" << err.message();
         return false;
-        }
+    }
 
     return true;
-    }
+}
 
 
 bool start()
     {
     std::auto_ptr<QDBusInterface> kded( Kded() );
-    if (!kded->isValid())
-        {
+    if (!kded->isValid()) {
         return false;
-        }
+    }
     QDBusReply<bool> reply = kded->call( "loadModule", "khotkeys"  );
     QDBusError err = reply.error();
 
-    if (err.isValid())
-        {
+    if (err.isValid()) {
         kError() << "Unable to start server org.kde.khotkeys (kded module) [" 
                  << err.name() << "]:" << err.message();
         return false;
-        }
+    }
 
     Q_ASSERT( reply.isValid() );
 
-    if ( reply.value() )
-        {
+    if ( reply.value() ) {
         return true;
-        }
-    else
-        {
+    } else {
         kError() << "Unable to start server org.kde.khotkeys (kded module)";
         return false;
-        }
     }
+}
 
 
 bool stop()
-    {
-    if (!isRunning())
-        {
+{
+    if (!isRunning()) {
         return true;
-        }
+    }
 
     std::auto_ptr<QDBusInterface> kded( Kded() );
-    if (!kded->isValid())
-        {
+    if (!kded->isValid()) {
         return false;
-        }
+    }
 
     QDBusReply<bool> reply = kded->call( "unloadModule", "khotkeys"  );
     QDBusError err = reply.error();
 
-    if (err.isValid())
-        {
-
+    if (err.isValid()) {
         kError() << "Error when stopping khotkeys kded module [" << err.name() << "]:" << err.message();
         return false;
-        }
+    }
 
     Q_ASSERT( reply.isValid() );
 
-    if ( reply.value() )
-        {
+    if ( reply.value() ) {
         return true;
-        }
-    else
-        {
+    } else {
         kError() << "Failed to stop server org.kde.khotkeys (kded module)";
         return false;
-        }
     }
+}
 
 }} // namespace KHotKeys::Daemon
