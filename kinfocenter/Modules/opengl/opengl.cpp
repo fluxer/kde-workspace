93111bcf7d188b58188b156a9b4b081d3adfab8e
Ivailo Monev <xakepa10@gmail.com>
2021-06-05 03:38:20 +0300

    kinfocenter: format OpenGL data tables
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kinfocenter/Modules/opengl/opengl.cpp
+++ kinfocenter/Modules/opengl/opengl.cpp
@@ -308,156 +308,156 @@ print_limits(QTreeWidgetItem *l1, const char * glExtensions, bool getProcAddress
     #define KCMGL_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
     const struct token_name various_limits[] = {
-        { 1, GL_MAX_LIGHTS, 		i18n("Max. number of light sources") },
-        { 1, GL_MAX_CLIP_PLANES,		i18n("Max. number of clipping planes") },
-        { 1, GL_MAX_PIXEL_MAP_TABLE, 	i18n("Max. pixel map table size") },
-        { 1, GL_MAX_LIST_NESTING, 	i18n("Max. display list nesting level") },
-        { 1, GL_MAX_EVAL_ORDER, 		i18n("Max. evaluator order") },
-        { 1, GL_MAX_ELEMENTS_VERTICES, 	i18n("Max. recommended vertex count") },
-        { 1, GL_MAX_ELEMENTS_INDICES, 	i18n("Max. recommended index count") },
+        { 1, GL_MAX_LIGHTS,               i18n("Max. number of light sources") },
+        { 1, GL_MAX_CLIP_PLANES,          i18n("Max. number of clipping planes") },
+        { 1, GL_MAX_PIXEL_MAP_TABLE,      i18n("Max. pixel map table size") },
+        { 1, GL_MAX_LIST_NESTING,         i18n("Max. display list nesting level") },
+        { 1, GL_MAX_EVAL_ORDER,           i18n("Max. evaluator order") },
+        { 1, GL_MAX_ELEMENTS_VERTICES,    i18n("Max. recommended vertex count") },
+        { 1, GL_MAX_ELEMENTS_INDICES,     i18n("Max. recommended index count") },
 #ifdef GL_QUERY_COUNTER_BITS
-        { 1, GL_QUERY_COUNTER_BITS, 	i18n("Occlusion query counter bits")},
+        { 1, GL_QUERY_COUNTER_BITS,       i18n("Occlusion query counter bits")},
 #endif
 #ifdef GL_MAX_VERTEX_UNITS_ARB
-        { 1, GL_MAX_VERTEX_UNITS_ARB, 	i18n("Max. vertex blend matrices") },
+        { 1, GL_MAX_VERTEX_UNITS_ARB,     i18n("Max. vertex blend matrices") },
 #endif
 #ifdef GL_MAX_PALETTE_MATRICES_ARB
         { 1, GL_MAX_PALETTE_MATRICES_ARB, i18n("Max. vertex blend matrix palette size") },
 #endif
-        {0,0,QString()}
+        { 0, 0, QString() }
     };
 
    const struct token_name texture_limits[] = {
-        { 1, GL_MAX_TEXTURE_SIZE, 	i18n("Max. texture size") },
-        { 1, GL_MAX_TEXTURE_UNITS_ARB, 	i18n("No. of texture units") },
-        { 1, GL_MAX_3D_TEXTURE_SIZE, 		i18n("Max. 3D texture size") },
-        { 1, GL_MAX_CUBE_MAP_TEXTURE_SIZE_ARB, 	i18n("Max. cube map texture size") },
+        { 1, GL_MAX_TEXTURE_SIZE,                       i18n("Max. texture size") },
+        { 1, GL_MAX_TEXTURE_UNITS_ARB,                  i18n("No. of texture units") },
+        { 1, GL_MAX_3D_TEXTURE_SIZE,                    i18n("Max. 3D texture size") },
+        { 1, GL_MAX_CUBE_MAP_TEXTURE_SIZE_ARB,          i18n("Max. cube map texture size") },
 #ifdef GL_MAX_RECTANGLE_TEXTURE_SIZE_NV
-        { 1, GL_MAX_RECTANGLE_TEXTURE_SIZE_NV, 	i18n("Max. rectangular texture size") },
+        { 1, GL_MAX_RECTANGLE_TEXTURE_SIZE_NV,          i18n("Max. rectangular texture size") },
 #endif
         { 1 | KCMGL_FLOAT, GL_MAX_TEXTURE_LOD_BIAS_EXT, i18n("Max. texture LOD bias") },
-        { 1, GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, 	i18n("Max. anisotropy filtering level") },
-        { 1, GL_NUM_COMPRESSED_TEXTURE_FORMATS_ARB, i18n("No. of compressed texture formats") },
-        {0,0,QString()}
+        { 1, GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT,         i18n("Max. anisotropy filtering level") },
+        { 1, GL_NUM_COMPRESSED_TEXTURE_FORMATS_ARB,     i18n("No. of compressed texture formats") },
+        { 0, 0, QString() }
     };
 
     const struct token_name float_limits[] = {
-        { 2 | KCMGL_FLOAT, GL_ALIASED_POINT_SIZE_RANGE, 	"ALIASED_POINT_SIZE_RANGE" },
-        { 2 | KCMGL_FLOAT, GL_SMOOTH_POINT_SIZE_RANGE, 	"SMOOTH_POINT_SIZE_RANGE" },
-        { 1 | KCMGL_FLOAT, GL_SMOOTH_POINT_SIZE_GRANULARITY,"SMOOTH_POINT_SIZE_GRANULARITY"},
-        { 2 | KCMGL_FLOAT, GL_ALIASED_LINE_WIDTH_RANGE, 	"ALIASED_LINE_WIDTH_RANGE" },
-        { 2 | KCMGL_FLOAT, GL_SMOOTH_LINE_WIDTH_RANGE, 	"SMOOTH_LINE_WIDTH_RANGE" },
-        { 1 | KCMGL_FLOAT, GL_SMOOTH_LINE_WIDTH_GRANULARITY,"SMOOTH_LINE_WIDTH_GRANULARITY"},
-        {0,0,QString()}
+        { 2 | KCMGL_FLOAT, GL_ALIASED_POINT_SIZE_RANGE,      "ALIASED_POINT_SIZE_RANGE" },
+        { 2 | KCMGL_FLOAT, GL_SMOOTH_POINT_SIZE_RANGE,       "SMOOTH_POINT_SIZE_RANGE" },
+        { 1 | KCMGL_FLOAT, GL_SMOOTH_POINT_SIZE_GRANULARITY, "SMOOTH_POINT_SIZE_GRANULARITY"},
+        { 2 | KCMGL_FLOAT, GL_ALIASED_LINE_WIDTH_RANGE,      "ALIASED_LINE_WIDTH_RANGE" },
+        { 2 | KCMGL_FLOAT, GL_SMOOTH_LINE_WIDTH_RANGE,       "SMOOTH_LINE_WIDTH_RANGE" },
+        { 1 | KCMGL_FLOAT, GL_SMOOTH_LINE_WIDTH_GRANULARITY, "SMOOTH_LINE_WIDTH_GRANULARITY"},
+        { 0, 0, QString() }
     };
 
     const struct token_name stack_depth[] = {
-        { 1, GL_MAX_MODELVIEW_STACK_DEPTH, 	"MAX_MODELVIEW_STACK_DEPTH" },
-        { 1, GL_MAX_PROJECTION_STACK_DEPTH, 	"MAX_PROJECTION_STACK_DEPTH" },
-        { 1, GL_MAX_TEXTURE_STACK_DEPTH, 		"MAX_TEXTURE_STACK_DEPTH" },
-        { 1, GL_MAX_NAME_STACK_DEPTH, 		"MAX_NAME_STACK_DEPTH" },
-        { 1, GL_MAX_ATTRIB_STACK_DEPTH, 		"MAX_ATTRIB_STACK_DEPTH" },
-        { 1, GL_MAX_CLIENT_ATTRIB_STACK_DEPTH, 	"MAX_CLIENT_ATTRIB_STACK_DEPTH" },
-        { 1, GL_MAX_COLOR_MATRIX_STACK_DEPTH, 	"MAX_COLOR_MATRIX_STACK_DEPTH" },
+        { 1, GL_MAX_MODELVIEW_STACK_DEPTH,          "MAX_MODELVIEW_STACK_DEPTH" },
+        { 1, GL_MAX_PROJECTION_STACK_DEPTH,         "MAX_PROJECTION_STACK_DEPTH" },
+        { 1, GL_MAX_TEXTURE_STACK_DEPTH,            "MAX_TEXTURE_STACK_DEPTH" },
+        { 1, GL_MAX_NAME_STACK_DEPTH,               "MAX_NAME_STACK_DEPTH" },
+        { 1, GL_MAX_ATTRIB_STACK_DEPTH,             "MAX_ATTRIB_STACK_DEPTH" },
+        { 1, GL_MAX_CLIENT_ATTRIB_STACK_DEPTH,      "MAX_CLIENT_ATTRIB_STACK_DEPTH" },
+        { 1, GL_MAX_COLOR_MATRIX_STACK_DEPTH,       "MAX_COLOR_MATRIX_STACK_DEPTH" },
 #ifdef GL_MAX_MATRIX_PALETTE_STACK_DEPTH_ARB
-        { 1, GL_MAX_MATRIX_PALETTE_STACK_DEPTH_ARB,"MAX_MATRIX_PALETTE_STACK_DEPTH"},
+        { 1, GL_MAX_MATRIX_PALETTE_STACK_DEPTH_ARB, "MAX_MATRIX_PALETTE_STACK_DEPTH"},
 #endif
-        {0,0,QString()}
+        { 0, 0, QString() }
     };
 
 #ifdef GL_ARB_fragment_program
     const struct token_name arb_fp[] = {
-        { 1, GL_MAX_TEXTURE_COORDS_ARB, "MAX_TEXTURE_COORDS" },
-        { 1, GL_MAX_TEXTURE_IMAGE_UNITS_ARB, "MAX_TEXTURE_IMAGE_UNITS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ENV_PARAMETERS_ARB, "MAX_PROGRAM_ENV_PARAMETERS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_LOCAL_PARAMETERS_ARB, "MAX_PROGRAM_LOCAL_PARAMETERS" },
-        { 1, GL_MAX_PROGRAM_MATRICES_ARB, "MAX_PROGRAM_MATRICES" },
-        { 1, GL_MAX_PROGRAM_MATRIX_STACK_DEPTH_ARB, "MAX_PROGRAM_MATRIX_STACK_DEPTH" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_INSTRUCTIONS_ARB, "MAX_PROGRAM_INSTRUCTIONS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ALU_INSTRUCTIONS_ARB, "MAX_PROGRAM_ALU_INSTRUCTIONS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEX_INSTRUCTIONS_ARB, "MAX_PROGRAM_TEX_INSTRUCTIONS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEX_INDIRECTIONS_ARB, "MAX_PROGRAM_TEX_INDIRECTIONS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEMPORARIES_ARB, "MAX_PROGRAM_TEMPORARIES" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_PARAMETERS_ARB, "MAX_PROGRAM_PARAMETERS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ATTRIBS_ARB, "MAX_PROGRAM_ATTRIBS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_INSTRUCTIONS_ARB, "MAX_PROGRAM_NATIVE_INSTRUCTIONS" },
+        { 1, GL_MAX_TEXTURE_COORDS_ARB,                               "MAX_TEXTURE_COORDS" },
+        { 1, GL_MAX_TEXTURE_IMAGE_UNITS_ARB,                          "MAX_TEXTURE_IMAGE_UNITS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ENV_PARAMETERS_ARB,          "MAX_PROGRAM_ENV_PARAMETERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_LOCAL_PARAMETERS_ARB,        "MAX_PROGRAM_LOCAL_PARAMETERS" },
+        { 1, GL_MAX_PROGRAM_MATRICES_ARB,                             "MAX_PROGRAM_MATRICES" },
+        { 1, GL_MAX_PROGRAM_MATRIX_STACK_DEPTH_ARB,                   "MAX_PROGRAM_MATRIX_STACK_DEPTH" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_INSTRUCTIONS_ARB,            "MAX_PROGRAM_INSTRUCTIONS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ALU_INSTRUCTIONS_ARB,        "MAX_PROGRAM_ALU_INSTRUCTIONS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEX_INSTRUCTIONS_ARB,        "MAX_PROGRAM_TEX_INSTRUCTIONS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEX_INDIRECTIONS_ARB,        "MAX_PROGRAM_TEX_INDIRECTIONS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEMPORARIES_ARB,             "MAX_PROGRAM_TEMPORARIES" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_PARAMETERS_ARB,              "MAX_PROGRAM_PARAMETERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ATTRIBS_ARB,                 "MAX_PROGRAM_ATTRIBS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_INSTRUCTIONS_ARB,     "MAX_PROGRAM_NATIVE_INSTRUCTIONS" },
         { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ALU_INSTRUCTIONS_ARB, "MAX_PROGRAM_NATIVE_ALU_INSTRUCTIONS" },
         { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_TEX_INSTRUCTIONS_ARB, "MAX_PROGRAM_NATIVE_TEX_INSTRUCTIONS" },
         { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_TEX_INDIRECTIONS_ARB, "MAX_PROGRAM_NATIVE_TEX_INDIRECTIONS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_TEMPORARIES_ARB, "MAX_PROGRAM_NATIVE_TEMPORARIES" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_PARAMETERS_ARB, "MAX_PROGRAM_NATIVE_PARAMETERS" },
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ATTRIBS_ARB, "MAX_PROGRAM_NATIVE_ATTRIBS" },
-        {0,0,QString()}
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_TEMPORARIES_ARB,      "MAX_PROGRAM_NATIVE_TEMPORARIES" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_PARAMETERS_ARB,       "MAX_PROGRAM_NATIVE_PARAMETERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ATTRIBS_ARB,          "MAX_PROGRAM_NATIVE_ATTRIBS" },
+        { 0, 0, QString() }
     };
 #endif
 
 #ifdef GL_ARB_vertex_program
     const struct token_name arb_vp[] = {
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ENV_PARAMETERS_ARB,"MAX_PROGRAM_ENV_PARAMETERS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_LOCAL_PARAMETERS_ARB,"MAX_PROGRAM_LOCAL_PARAMETERS"},
-        { 1, GL_MAX_VERTEX_ATTRIBS_ARB, "MAX_VERTEX_ATTRIBS"},
-        { 1, GL_MAX_PROGRAM_MATRICES_ARB,"MAX_PROGRAM_MATRICES"},
-        { 1, GL_MAX_PROGRAM_MATRIX_STACK_DEPTH_ARB,"MAX_PROGRAM_MATRIX_STACK_DEPTH"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_INSTRUCTIONS_ARB,"MAX_PROGRAM_INSTRUCTIONS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEMPORARIES_ARB,"MAX_PROGRAM_TEMPORARIES"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_PARAMETERS_ARB,"MAX_PROGRAM_PARAMETERS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ATTRIBS_ARB,"MAX_PROGRAM_ATTRIBS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ADDRESS_REGISTERS_ARB,"MAX_PROGRAM_ADDRESS_REGISTERS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_INSTRUCTIONS_ARB,"MAX_PROGRAM_NATIVE_INSTRUCTIONS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_TEMPORARIES_ARB,"MAX_PROGRAM_NATIVE_TEMPORARIES"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_PARAMETERS_ARB,"MAX_PROGRAM_NATIVE_PARAMETERS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ATTRIBS_ARB,"MAX_PROGRAM_NATIVE_ATTRIBS"},
-        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ADDRESS_REGISTERS_ARB ,"MAX_PROGRAM_NATIVE_ADDRESS_REGISTERS"},
-        {0,0,QString()}
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ENV_PARAMETERS_ARB,           "MAX_PROGRAM_ENV_PARAMETERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_LOCAL_PARAMETERS_ARB,         "MAX_PROGRAM_LOCAL_PARAMETERS" },
+        { 1, GL_MAX_VERTEX_ATTRIBS_ARB,                                "MAX_VERTEX_ATTRIBS" },
+        { 1, GL_MAX_PROGRAM_MATRICES_ARB,                              "MAX_PROGRAM_MATRICES" },
+        { 1, GL_MAX_PROGRAM_MATRIX_STACK_DEPTH_ARB,                    "MAX_PROGRAM_MATRIX_STACK_DEPTH" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_INSTRUCTIONS_ARB,             "MAX_PROGRAM_INSTRUCTIONS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_TEMPORARIES_ARB,              "MAX_PROGRAM_TEMPORARIES" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_PARAMETERS_ARB,               "MAX_PROGRAM_PARAMETERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ATTRIBS_ARB,                  "MAX_PROGRAM_ATTRIBS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_ADDRESS_REGISTERS_ARB,        "MAX_PROGRAM_ADDRESS_REGISTERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_INSTRUCTIONS_ARB,      "MAX_PROGRAM_NATIVE_INSTRUCTIONS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_TEMPORARIES_ARB,       "MAX_PROGRAM_NATIVE_TEMPORARIES" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_PARAMETERS_ARB,        "MAX_PROGRAM_NATIVE_PARAMETERS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ATTRIBS_ARB,           "MAX_PROGRAM_NATIVE_ATTRIBS" },
+        { 1 | KCMGL_PROG, GL_MAX_PROGRAM_NATIVE_ADDRESS_REGISTERS_ARB, "MAX_PROGRAM_NATIVE_ADDRESS_REGISTERS" },
+        { 0, 0, QString() }
     };
 #endif
 
 #ifdef GL_ARB_vertex_shader
     const struct token_name arb_vs[] = {
-        { 1, GL_MAX_VERTEX_ATTRIBS_ARB,"MAX_VERTEX_ATTRIBS"},
-        { 1, GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB,"MAX_VERTEX_UNIFORM_COMPONENTS"},
-        { 1, GL_MAX_VARYING_FLOATS_ARB,"MAX_VARYING_FLOATS"},
-        { 1, GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS_ARB,"MAX_COMBINED_TEXTURE_IMAGE_UNITS"},
-        { 1, GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS_ARB,"MAX_VERTEX_TEXTURE_IMAGE_UNITS"},
-        { 1, GL_MAX_TEXTURE_IMAGE_UNITS_ARB,"MAX_TEXTURE_IMAGE_UNITS"},
-        { 1, GL_MAX_TEXTURE_COORDS_ARB,"MAX_TEXTURE_COORDS"},
-        {0,0,QString()}
+        { 1, GL_MAX_VERTEX_ATTRIBS_ARB,               "MAX_VERTEX_ATTRIBS" },
+        { 1, GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB,    "MAX_VERTEX_UNIFORM_COMPONENTS" },
+        { 1, GL_MAX_VARYING_FLOATS_ARB,               "MAX_VARYING_FLOATS" },
+        { 1, GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS_ARB, "MAX_COMBINED_TEXTURE_IMAGE_UNITS" },
+        { 1, GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS_ARB,   "MAX_VERTEX_TEXTURE_IMAGE_UNITS" },
+        { 1, GL_MAX_TEXTURE_IMAGE_UNITS_ARB,          "MAX_TEXTURE_IMAGE_UNITS" },
+        { 1, GL_MAX_TEXTURE_COORDS_ARB,               "MAX_TEXTURE_COORDS" },
+        { 0, 0, QString()}
     };
 #endif
 
 #ifdef GL_ARB_fragment_shader
     const struct token_name arb_fs[] = {
-        { 1, GL_MAX_FRAGMENT_UNIFORM_COMPONENTS_ARB,"MAX_FRAGMENT_UNIFORM_COMPONENTS"},
-        { 1, GL_MAX_TEXTURE_IMAGE_UNITS_ARB,"MAX_TEXTURE_IMAGE_UNITS"},
-        { 1, GL_MAX_TEXTURE_COORDS_ARB,"MAX_TEXTURE_COORDS"},
-        {0,0,QString()}
+        { 1, GL_MAX_FRAGMENT_UNIFORM_COMPONENTS_ARB, "MAX_FRAGMENT_UNIFORM_COMPONENTS" },
+        { 1, GL_MAX_TEXTURE_IMAGE_UNITS_ARB,         "MAX_TEXTURE_IMAGE_UNITS" },
+        { 1, GL_MAX_TEXTURE_COORDS_ARB,              "MAX_TEXTURE_COORDS" },
+        { 0, 0, QString() }
     };
 #endif
 
     const struct token_name frame_buffer_props[] = {
-        { 2, GL_MAX_VIEWPORT_DIMS, 	i18n("Max. viewport dimensions") },
-        { 1, GL_SUBPIXEL_BITS, 		i18n("Subpixel bits") },
-        { 1, GL_AUX_BUFFERS, 		i18n("Aux. buffers")},
-        {0,0,QString()}
+        { 2, GL_MAX_VIEWPORT_DIMS, i18n("Max. viewport dimensions") },
+        { 1, GL_SUBPIXEL_BITS,     i18n("Subpixel bits") },
+        { 1, GL_AUX_BUFFERS,       i18n("Aux. buffers") },
+        { 0, 0, QString() }
     };
 
     const struct token_group groups[] = {
-        {KCMGL_SIZE(frame_buffer_props), 0, frame_buffer_props, i18n("Frame buffer properties"), NULL},
-        {KCMGL_SIZE(various_limits), 0, texture_limits, i18n("Texturing"), NULL},
-        {KCMGL_SIZE(various_limits), 0, various_limits, i18n("Various limits"), NULL},
-        {KCMGL_SIZE(float_limits), 0, float_limits, i18n("Points and lines"), NULL},
-        {KCMGL_SIZE(stack_depth), 0, stack_depth, i18n("Stack depth limits"), NULL},
+        { KCMGL_SIZE(frame_buffer_props), 0,                       frame_buffer_props, i18n("Frame buffer properties"), NULL },
+        { KCMGL_SIZE(various_limits),     0,                       texture_limits,     i18n("Texturing"),               NULL },
+        { KCMGL_SIZE(various_limits),     0,                       various_limits,     i18n("Various limits"),          NULL },
+        { KCMGL_SIZE(float_limits),       0,                       float_limits,       i18n("Points and lines"),        NULL},
+        { KCMGL_SIZE(stack_depth),        0,                       stack_depth,        i18n("Stack depth limits"),      NULL},
 #ifdef GL_ARB_vertex_program
-        {KCMGL_SIZE(arb_vp), GL_VERTEX_PROGRAM_ARB, arb_vp, "ARB_vertex_program", "GL_ARB_vertex_program"},
+        { KCMGL_SIZE(arb_vp),             GL_VERTEX_PROGRAM_ARB,   arb_vp,             "ARB_vertex_program",            "GL_ARB_vertex_program" },
 #endif
 #ifdef GL_ARB_fragment_program
-        {KCMGL_SIZE(arb_fp), GL_FRAGMENT_PROGRAM_ARB, arb_fp, "ARB_fragment_program", "GL_ARB_fragment_program"},
+        { KCMGL_SIZE(arb_fp),             GL_FRAGMENT_PROGRAM_ARB, arb_fp,             "ARB_fragment_program",          "GL_ARB_fragment_program" },
 #endif
 #ifdef GL_ARB_vertex_shader
-        {KCMGL_SIZE(arb_vs), 0, arb_vs, "ARB_vertex_shader", "GL_ARB_vertex_shader"},
+        { KCMGL_SIZE(arb_vs),             0,                       arb_vs,             "ARB_vertex_shader",             "GL_ARB_vertex_shader" },
 #endif
 #ifdef GL_ARB_fragment_shader
-        {KCMGL_SIZE(arb_fs), 0, arb_fs, "ARB_fragment_shader", "GL_ARB_fragment_shader"},
+        { KCMGL_SIZE(arb_fs),             0,                       arb_fs,             "ARB_fragment_shader",           "GL_ARB_fragment_shader" },
 #endif
     };
 
