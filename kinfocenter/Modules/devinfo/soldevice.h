18b4287ea19dd68129be16f38f8cfe8f7e5eaaa7
Ivailo Monev <xakepa10@gmail.com>
2021-06-10 22:44:29 +0300

    kinfocenter: warning on solid device cast failure
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kinfocenter/Modules/devinfo/soldevice.h
+++ kinfocenter/Modules/devinfo/soldevice.h
@@ -77,7 +77,7 @@ class SolDevice : public QTreeWidgetItem
 	IFace *dev = tiedDevice.as<const IFace>();
 	if(!dev)
 	{
-	  kDebug() << i18n("Device unable to be cast to correct device") << endl;
+	  kWarning() << i18n("Device unable to be cast to correct device");
 	}
 	return dev;
       } else {
@@ -89,7 +89,7 @@ class SolDevice : public QTreeWidgetItem
     {
 	IFace *dev = device.as<const IFace>();
 	if(!dev) {
-	  kDebug() << i18n("Device unable to be cast to correct device") << endl;
+	  kWarning() << i18n("Device unable to be cast to correct device");
 	}
 	return dev;
     }
