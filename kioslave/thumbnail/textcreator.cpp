9748c4adf59418d19e3b1ef043a775fbdc157146
Ivailo Monev <xakepa10@gmail.com>
2021-09-02 03:04:41 +0300

    kioslave: remove unused variable
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kioslave/thumbnail/textcreator.cpp
+++ kioslave/thumbnail/textcreator.cpp
@@ -135,8 +135,6 @@ bool TextCreator::create(const QString &path, int width, int height, QImage &img
 #endif
             m_pixmap.fill( bgColor );
 
-            QRect rect;
-
             QPainter painter( &m_pixmap );
             painter.setFont( font );
             painter.setPen( fgColor );
