fluxer/kde-workspace

commit 7d2095df32800f4c460f71314d707cc864f6c2d0

generic: replace QLinkedList<T> with QList<T>
Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

plasma/applets/kickoff/core/recentapplications.cpp
@@ -22,7 +22,7 @@
 
 // Qt
 #include <QtCore/QHash>
-#include <QtCore/QLinkedList>
+#include <QtCore/QList>
 
 // KDE
 #include <KConfigGroup>
@@ -107,7 +107,7 @@ public:
         QString storageId;
         int startCount;
         QDateTime lastStartedTime;
-        QLinkedList<QString>::iterator queueIter;
+        QList<QString>::iterator queueIter;
 
         bool operator<(const ServiceInfo& rhs) const {
             return this->lastStartedTime < rhs.lastStartedTime;
@@ -118,7 +118,7 @@ public:
     int defaultMaxServices, maxServices;
     // queue to keep track of the order in which services have been used
     // (most recently used at the back)
-    QLinkedList<QString> serviceQueue;
+    QList<QString> serviceQueue;
     QHash<QString, ServiceInfo> serviceInfo;
     RecentApplications instance;
 };

kwin/effects/presentwindows/presentwindows.cpp
@@ -1012,7 +1012,7 @@ void PresentWindowsEffect::calculateWindowTransformationsClosest(EffectWindowLis
         }
 
     // Assign each window to the closest available slot
-    EffectWindowList tmpList = windowlist; // use a QLinkedList copy instead?
+    EffectWindowList tmpList = windowlist; // use a QList copy instead?
     QPoint otherPos;
     while (!tmpList.isEmpty()) {
         EffectWindow *w = tmpList.first();

commit a2ca134e84c33cde0a1e97777072d2b47dde09c5

plasma: fix rare crash on applications launch from kickoff menu
regression since 7d2095df32800f4c460f71314d707cc864f6c2d0

Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

plasma/applets/kickoff/core/recentapplications.cpp
@@ -80,11 +80,10 @@ public:
         // queue entry (so that there are no duplicates in the queue)
         if (serviceInfo.contains(id)) {
             kDebug() << "Duplicate entry added.  Removing existing entry from queue.";
-            serviceQueue.erase(serviceInfo[id].queueIter);
+            serviceQueue.removeAll(id);
         }
 
         serviceQueue.append(id);
-        info.queueIter = --serviceQueue.end();
         serviceInfo.insert(id, info);
     }
 
@@ -107,7 +106,6 @@ public:
         QString storageId;
         int startCount;
         QDateTime lastStartedTime;
-        QList<QString>::iterator queueIter;
 
         bool operator<(const ServiceInfo& rhs) const {
             return this->lastStartedTime < rhs.lastStartedTime;