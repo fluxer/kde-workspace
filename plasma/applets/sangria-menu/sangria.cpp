045a96440290e7109a2412974f991b75d737c76f
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-07-21 14:50:15 -0400

    fix my last name and replace email with cleaner one
    chazpelo2 has been filled with bloated spam for nearly 7 years, doesn't help that it's linked to my youtubes. chazizcontact is more professional and has no bloat (besides twitter shit but i can archive all of that)

--- plasma/applets/sangria-menu/sangria.cpp
+++ plasma/applets/sangria-menu/sangria.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright 2022 by Chaz Peloquin <chazpelo2@gmail.com>                 *
+ *   Copyright 2022 by Chaz Péloquin <chazizcontact@gmail.com              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
