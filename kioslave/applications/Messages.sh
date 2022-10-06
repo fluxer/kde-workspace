24fa558df43c381480f7a39f7690aebd4fb03aec
Ivailo Monev <xakepa10@gmail.com>
2022-10-06 14:03:03 +0300

    kioslave: remove archive, recentdocuments, settings and applicaitons KIO slaves
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kioslave/applications/Messages.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$XGETTEXT `find . -name "*.cc" -o -name "*.cpp" -o -name "*.h"` -o $podir/kio_applications.pot
--- kioslave/settings/Messages.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$XGETTEXT `find . -name "*.cc" -o -name "*.cpp" -o -name "*.h"` -o $podir/kio_settings.pot
