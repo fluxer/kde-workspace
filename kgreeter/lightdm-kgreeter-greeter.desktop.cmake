34b6e2389deeeb3b1c95cabd3b30d68b5e6cd288
Ivailo Monev <xakepa10@gmail.com>
2022-12-06 10:47:16 +0200

    kgreeter: set Exec to the full path to lightdm-kgreeter-greeter executable
    fixes greeter start in some cases (such as when PATH does not include
    /usr/sbin by default even if the current user is root, e.g. on Arch Linux)
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kgreeter/lightdm-kgreeter-greeter.desktop.cmake
+++ kgreeter/lightdm-kgreeter-greeter.desktop.cmake
@@ -1,6 +1,6 @@
 [Desktop Entry]
 Name=LightDM KGreeter Greeter
 Comment=This runs the KGreeter greeter, it should only be run from LightDM
-Exec=lightdm-kgreeter-greeter
+Exec=@KDE4_SBIN_INSTALL_DIR@/lightdm-kgreeter-greeter
 Type=Application
 X-Ubuntu-Gettext-Domain=lightdm
