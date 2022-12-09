0b6cf10b597042dd18675058105bef4e5e6a3841
Ivailo Monev <xakepa10@gmail.com>
2022-12-09 12:54:09 +0200

    startkde: do not wait for drkonqi when shutting down
    ksmserver logs backtraces on crash now, the rest are on their own
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- startkde.cmake
+++ startkde.cmake
@@ -161,25 +161,6 @@ if test $? -ne 0; then
     xmessage -geometry 500x100 "Could not start ksmserver. Check your installation."
 fi
 
-wait_drkonqi=`kreadconfig --file startkderc --group WaitForDrKonqi --key Enabled --default true`
-
-if test x"$wait_drkonqi"x = x"true"x ; then
-    # wait for remaining drkonqi instances with timeout (in seconds)
-    wait_drkonqi_timeout=`kreadconfig --file startkderc --group WaitForDrKonqi --key Timeout --default 900`
-    wait_drkonqi_counter=0
-    while $qdbus | grep "^[^w]*org.kde.drkonqi" > /dev/null ; do
-        sleep 5
-        wait_drkonqi_counter=$((wait_drkonqi_counter+5))
-        if test "$wait_drkonqi_counter" -ge "$wait_drkonqi_timeout" ; then
-            # ask remaining drkonqis to die in a graceful way
-            $qdbus | grep 'org.kde.drkonqi-' | while read address ; do
-                $qdbus "$address" "/MainApplication" "quit"
-            done
-            break
-        fi
-    done
-fi
-
 echo 'startkde: Shutting down...'  1>&2
 
 # Clean up
