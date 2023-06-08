f1fe166b7c24c89ce817bc262a389784bb334a32
Ivailo Monev <xakepa10@gmail.com>
2023-06-08 13:03:37 +0300

    libs: copyright favicons KDED module to me
    rewrote it
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- libs/konq/favicons/favicons.h
+++ libs/konq/favicons/favicons.h
@@ -1,19 +1,19 @@
-/* This file is part of the KDE Project
-   Copyright (c) 2001 Malte Starostik <malte@kde.org>
+/*  This file is part of the KDE project
+    Copyright (C) 2023 Ivailo Monev <xakepa10@gmail.com>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2, as published by the Free Software Foundation.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
 */
 
 #ifndef _FAVICONS_H_
@@ -27,8 +27,6 @@ class FavIconsModulePrivate;
 /**
  * FavIconsModule implements a KDED Module that downloads shortcut icons for
  * URLs and stores them on disk for later use.
- *
- * @author Malte Starostik <malte@kde.org>
  */
 class FavIconsModule : public KDEDModule
 {
