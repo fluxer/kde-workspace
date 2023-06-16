ec2d106e5c50b9d46e594978021913e479ccf130
Ivailo Monev <xakepa10@gmail.com>
2023-06-16 18:49:54 +0300

    kwin: remove unused KWin::MockTabBoxHandler::outlineWindowIds() method
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kwin/tabbox/tests/mock_tabboxhandler.h
+++ kwin/tabbox/tests/mock_tabboxhandler.h
@@ -68,9 +68,6 @@ public:
     virtual int numberOfDesktops() const {
         return 1;
     }
-    virtual QVector< xcb_window_t > outlineWindowIds() const {
-        return QVector<xcb_window_t>();
-    }
     virtual bool isKWinCompositing() const {
         return false;
     }
