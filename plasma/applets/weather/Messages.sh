70408d36d6af32f5fee9bafea62bdd31abbf7564
Ivailo Monev <xakepa10@gmail.com>
2023-09-23 16:50:07 +0300

    plasma: add script to extract translations for the weather applet
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ plasma/applets/weather/Messages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_weather.pot
+rm -f rc.cpp
